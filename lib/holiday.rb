cbc475b2afa9e6b99c8c8200bdf43f4aa82dae1d
Ali Karimi <ajkarimi7@gmail.com>
2019-09-11 18:52:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,6 +76,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
+    end 
       
     
 
